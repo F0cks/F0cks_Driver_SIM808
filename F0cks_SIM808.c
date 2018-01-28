9856967b517306549fcdfaaabdd27ca6be26f606
F0cks <enjolras.g@gmail.com>
2018-01-28 15:30:30 +0100

    Power ON/OFF procedure with PWRKEY

--- F0cks_SIM808.c
+++ F0cks_SIM808.c
@@ -1,1 +1,26 @@
 #include "F0cks_SIM808.h"
+
+int8_t F0cks_SIM808_Init( SIM808_HandleTypeDef *handler, SIM808_ConfigurationTypeDef config)
+{
+	int8_t error = 0;
+
+	return error;
+}
+
+/* Power ON SIM808 using PWRKEY */
+void F0cks_SIM808_Power_ON()
+{
+	F0cks_SIM808_PWRKEY_High();
+	F0cks_Delay_ms(500);
+	F0cks_SIM808_PWRKEY_Low();
+	F0cks_Delay_ms(1000);
+	F0cks_SIM808_PWRKEY_High();
+}
+
+/* Power OFF SIM808 using PWRKEY */
+void F0cks_SIM808_Power_OFF()
+{
+	F0cks_SIM808_PWRKEY_Low();
+	F0cks_Delay_ms(2000);
+	F0cks_SIM808_PWRKEY_High();
+}
--- F0cks_SIM808.h
+++ F0cks_SIM808.h
@@ -17,5 +17,12 @@ typedef struct
 }SIM808_HandleTypeDef;
 
 int8_t F0cks_SIM808_Init( SIM808_HandleTypeDef *handler, SIM808_ConfigurationTypeDef config);
+void F0cks_SIM808_Power_ON(void);
+void F0cks_SIM808_Power_OFF(void);
+
+/* Functions to implement */
+void F0cks_Delay_ms(uint32_t ms);
+void F0cks_SIM808_PWRKEY_High(void);
+void F0cks_SIM808_PWRKEY_Low(void);
 
 #endif //F0CKS_SIM808_H
