a69dbdfe52d986d7b519733872b7ff5d53d2cbd2
F0cks <enjolras.g@gmail.com>
2018-02-09 10:28:55 +0100

    Get strings in circular buffer

--- F0cks_SIM808.c
+++ F0cks_SIM808.c
@@ -1,10 +1,13 @@
 #include "F0cks_SIM808.h"
+#include "usart.h"
 
 int8_t F0cks_SIM808_Init( SIM808_HandleTypeDef *handler, SIM808_ConfigurationTypeDef config)
 {
 	int8_t error = 0;
 	uint8_t i = 0;
-	char *p;
+	uint8_t *p;
+
+	handler->ack = 0;
 
 	/* Get UART Circular Buffer data from user */
 	handler->uartCircularBuffer     = config.uartCircularBuffer;
@@ -19,25 +22,94 @@ int8_t F0cks_SIM808_Init( SIM808_HandleTypeDef *handler, SIM808_ConfigurationTyp
 		*p = '\200';
 		p++;
 	}
+	*config.uartCircularBuffer = 0;
 
+	for(i=0; i< STRING_BUFFER_SIZE; i++)
+	{
+		handler->privateStringBuffer[i] = '\0';
+	}
 
 	return error;
 }
 
 /* Power ON SIM808 using PWRKEY */
-void F0cks_SIM808_Power_ON()
+void F0cks_SIM808_Power_ON(SIM808_HandleTypeDef *handler)
 {
+	/* Start module with PWRKEY */
 	F0cks_SIM808_PWRKEY_High();
 	F0cks_Delay_ms(500);
 	F0cks_SIM808_PWRKEY_Low();
 	F0cks_Delay_ms(1000);
 	F0cks_SIM808_PWRKEY_High();
+
+	/* Initialize UART communication */
+	F0cks_SIM808_UART_Send("AT\n\r");// Get first dummy answer
+	F0cks_Delay_ms(2000);
+	F0cks_SIM808_UART_Send("AT\n\r");
+	while(1)
+	{
+		F0cks_Read_Circular_Buffer(handler);
+	}
+
 }
 
 /* Power OFF SIM808 using PWRKEY */
 void F0cks_SIM808_Power_OFF()
 {
+	/* Stop module with PWRKEY */
 	F0cks_SIM808_PWRKEY_Low();
 	F0cks_Delay_ms(2000);
 	F0cks_SIM808_PWRKEY_High();
 }
+
+/* Read Circular buffer */
+int8_t F0cks_Read_Circular_Buffer(SIM808_HandleTypeDef *handler)
+{
+	uint8_t timeout = 0;
+	uint8_t *p = handler->privateStringBuffer;
+	uint8_t currentChar = '\0',
+	        lastChar    = '\0';
+
+	while(timeout <= 100)
+	{
+		if( *handler->privateCircularBufferP != '\200' )
+		{
+			/* Reset timeout */
+			timeout = 0;
+
+			/* Store current and last char */
+			lastChar    = currentChar;
+			currentChar = *handler->privateCircularBufferP;
+
+			/* Do not store \n and \r */
+			if( *handler->privateCircularBufferP != '\n' &&
+					*handler->privateCircularBufferP != '\r' &&
+					*handler->privateCircularBufferP != 0xff &&
+					*handler->privateCircularBufferP != '\0' )
+			{
+				*p = *handler->privateCircularBufferP;
+				p++;
+			}
+
+			/* Move to next char */
+			*handler->privateCircularBufferP = '\200';
+			handler->privateCircularBufferP++;
+
+			if(currentChar == '\n' && lastChar == '\r')
+			{
+				/* New string */
+				return 1;
+			}
+		}
+		else
+		{
+			/* Increase timeout */
+			timeout++;
+			F0cks_Delay_ms(10);
+		}
+	}
+
+	/* Timeout: No new string */
+	return 0;
+}
+
--- F0cks_SIM808.h
+++ F0cks_SIM808.h
@@ -3,9 +3,11 @@
 
 #include "stdint.h"
 
+#define STRING_BUFFER_SIZE 100
+
 typedef struct
 {
-	char *  uartCircularBuffer;
+	uint8_t *  uartCircularBuffer;
 	uint8_t uartCircularBufferSize;
 
 }SIM808_ConfigurationTypeDef;
@@ -15,20 +17,24 @@ typedef struct
   */
 typedef struct
 {
-	char *  uartCircularBuffer;
+	uint8_t *  uartCircularBuffer;
 	uint8_t uartCircularBufferSize;
 
-	char *  privateCircularBufferP;
+	uint8_t *  privateCircularBufferP;
+	uint8_t    privateStringBuffer[STRING_BUFFER_SIZE];
+	int8_t  ack;
 
 }SIM808_HandleTypeDef;
 
 int8_t F0cks_SIM808_Init( SIM808_HandleTypeDef *handler, SIM808_ConfigurationTypeDef config);
-void F0cks_SIM808_Power_ON(void);
+void F0cks_SIM808_Power_ON(SIM808_HandleTypeDef *handler);
 void F0cks_SIM808_Power_OFF(void);
+int8_t F0cks_Read_Circular_Buffer(SIM808_HandleTypeDef *handler);
 
 /* Functions to implement */
 void F0cks_Delay_ms(uint32_t ms);
 void F0cks_SIM808_PWRKEY_High(void);
 void F0cks_SIM808_PWRKEY_Low(void);
+void F0cks_SIM808_UART_Send(char *string);
 
 #endif //F0CKS_SIM808_H
